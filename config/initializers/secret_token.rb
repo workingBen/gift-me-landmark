b951bdabfae603bcf11104004125100bead7b82f
Ben Pellow <benjamin.pellow@gmail.com>
2012-01-31 00:12:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wantwillwont::Application.config.secret_token = 'c93ea09aee68c1fbe0bd286486c423d76897ed20d2c0697613e630d68603ae31c5f19c41be5f2a7ebad17b585afc2572a6ad4a87977f2077e20034afe2a24f97'
